35ebf16109620dda98b9c5c75159e7d02a53f438
alex90631 <alex90631@cs104u-2a.acad.fullerton.edu>
2019-09-28 13:34:08 -0700

    Project 2

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include <sstream>
 
 int main()
 {
@@ -11,6 +12,7 @@ int main()
   double total_hotel_expenses;
   double total_meal_expense;
   double total_expense;
+  std::stringstream sshotel, ssmeal, sstotal;
 
   std:: cout << "Welcome to the Business Trip Tracker!\n\n";
   std:: cout << "What is the business trip location? ";// Program beings by asking the user to input the US location where the trip was taken
@@ -26,17 +28,21 @@ int main()
 
   total_expense = total_hotel_expenses + total_meal_expense;// Opeartion calculates total expene cost by multiplying 2 different inputs
 
+  sshotel << std:: setprecision(2) << std::fixed << '$' << total_hotel_expenses; //string sstream
+  ssmeal << std:: setprecision(2) << std::fixed << '$' << total_meal_expense; //string sstream
+  sstotal << std:: setprecision(2) << std::fixed << '$' << total_expense; //string sstream
+
   std::cout << "Location"// This is where it starts displaying the output results
-  << std:: setw(21) << "Days"
-  << std:: setw(19) << "Hotel"
-  << std:: setw(18) << "Meal"
-  << std:: setw(19) << "Total\n";
+    << std:: setw(16) << "Days"
+    << std:: setw(16) << "Hotel"
+    << std:: setw(16) << "Meal"
+    << std:: setw(16) << "Total\n";
 
   std::cout << location// All calculations are displayed in order and horizontally
-  << std:: setw(16) << total_days
-  << std:: setw(16) << std:: fixed << std:: setprecision(2) << "$" << total_hotel_expenses
-  << std:: setw(14) << "$" << total_meal_expense
-  << std:: setw(13) << "$" << total_expense << std::endl;
+    << std:: setw(16) << total_days
+    << std:: setw(16) << std:: fixed << std:: setprecision(2) << sshotel.str()
+    << std:: setw(16) << ssmeal.str()
+    << std:: setw(16) << sstotal.str() << std:: endl;
 
 
   return 0;
